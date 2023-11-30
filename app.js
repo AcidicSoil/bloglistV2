d18795fb725f2ff54493f7d9fd56b7f69092f87e
AcidicSoil <claytonbivens1@gmail.com>
2023-11-30 08:46:29 -0600

    uibuild

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // app.js
 const config = require("./utils/config")
 const express = require("express")
-require("express-async-errors")
 const app = express()
 const cors = require("cors")
 const blogsRouter = require("./controllers/blogs")
@@ -13,19 +12,23 @@ mongoose.set("strictQuery", false)
 
 logger.info("connecting to", config.MONGODB_URI)
 
-const { requestLogger, unknownEndpoint, errorHandler } = require("./utils/middleware")
-
-
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB")
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message)
+  })
 
 app.use(cors())
 app.use(express.static("dist"))
 app.use(express.json())
-app.use(requestLogger)
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
 
